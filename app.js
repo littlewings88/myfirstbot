6f09c3de62cf05f2f8b7099ba599e1c623b0c367
Ju dy <Ju dy>
2016-10-23 17:57:55 +0800

    v3

--- app.js
+++ app.js
@@ -19,41 +19,73 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=27980a6e-ec18-4fa5-bc3f-8a031eb74f4c&subscription-key=a5c9c598a1864e928073f34258f04e27';
+// Main dialog with LUIS
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+    .matches('FindActivity', [
+        function (session, args, next) {
+         
 
-var intents = new builder.IntentDialog();
+            // try extracting entities
+            var cityEntity = builder.EntityRecognizer.findEntity(args.entities, 'location');
+           
+            if (cityEntity) {
+                // city entity detected, continue to next step
+                session.dialogData.searchType = 'location';
+                next({ response: cityEntity.entity });
+            }  else {
+                // no entities detected, ask user for a destination
+                builder.Prompts.text(session, 'Could not detect a location');
+            }
+        },
+        function (session, results) {
+            var destination = results.response;
+
+            var message = 'Looking for carpark';
+            if (session.dialogData.searchType === 'airport') {
+                message += ' near %s airport...';
+            } else {
+                message += ' in %s...';
+            }
+
+            session.send(message, destination);
+
+            var message = new builder.Message()
+                        .attachmentLayout(builder.AttachmentLayout.carousel)
+                        .attachments(cityEntity.map(hotelAsAttachment));
+
+                    session.send(message);
+
+                    // End
+                    session.endDialog();
+
+            
+        }
+    ])
+
+  
+    .onDefault((session) => {
+        session.send('Sorry, I did not understand \'%s\'. ', session.message.text);
+    });
+
 bot.dialog('/', intents);
 
-intents.matches(/^change name/i, [
-    function (session) {
-        session.beginDialog('/profile');
-    },
-    function (session, results) {
-        session.send('Ok... Changed your name to %s', session.userData.name);
-    }
-]);
+// Helpers
+function hotelAsAttachment(hotel) {
+    return new builder.HeroCard()
+        .buttons([
+            new builder.CardAction()
+                .title('More details')
+                .type('openUrl')
+                .value('https://www.bing.com/search?q=' + encodeURIComponent(cityEntity.location))
+        ]);
+}
 
-intents.onDefault([
-    function (session, args, next) {
-        if (!session.userData.name) {
-            session.beginDialog('/profile');
-        } else {
-            next();
-        }
-    },
-    function (session, results) {
-        session.send('Hello %s!', session.userData.name);
-    }
-]);
-
-bot.dialog('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
-    }
-]);
+function reviewAsAttachment(review) {
+    return new builder.ThumbnailCard()
+        .title(review.title)
+        .text(review.text)
+        .images([new builder.CardImage().url(review.image)])
+}
