f7313050c3e991e9f7dd248459240f12df7e3a25
Ju dy <Ju dy>
2016-10-25 16:58:51 +0800

    v6

--- app.js
+++ app.js
@@ -88,6 +88,7 @@ intents.matches('FindActivity', [
                 //assert.ok(response[0].faceId);
                 //session.send(response[0].faceAttributes.gender);
 				billFaces.push(response[0].faceId);
+				session.send(response[0].faceId);
                 
             }).catch(function (error) {
                 // Check if subscription is valid
